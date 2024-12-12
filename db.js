760b8fc63adc6df5d4f90015f958409aa8ee6962
SashankSingh97 <129739180+SashankSingh97@users.noreply.github.com>
2024-12-12 23:26:19 +0530

    db server

--- db.js
+++ db.js
@@ -4,14 +4,17 @@ dotenv.config({ path: "./.env" });
 
 const connectDB = async () => {
   try {
+    console.log('Connecting to MongoDB...');
     const conn = await mongoose.connect('mongodb+srv://singhsashank:DPY6hb8eFTSfmyZM@clusterhrm.jpu4rnt.mongodb.net/', {
       dbName: "chatapp",
+      useNewUrlParser: true, // You can also use `useUnifiedTopology` if necessary
+      useUnifiedTopology: true,
     });
 
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    process.exit(1); // Exit the process if MongoDB connection fails
   }
 };
 
